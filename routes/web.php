96dbaeaea1ad64baea0eb3a7cb081c4b4b1c1bd5
Kian <kheijhackt@gmail.com>
2026-05-09 08:20:14 +0800

    Fixed pint error

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,10 @@ use Illuminate\Support\Str;
 
 Route::get('/', function () {
     $path = base_path('README.md');
-    
-    $content = file_exists($path) ? file_get_contents($path) : "# File not found";
-    
+
+    $content = file_exists($path) ? file_get_contents($path) : '# File not found';
+
     return view('welcome', [
-        'htmlContent' => Str::markdown($content)
+        'htmlContent' => Str::markdown($content),
     ]);
 });
